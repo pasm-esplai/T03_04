e378d640f78b1c42ac1076cfe78d11e28b260775
Usuario <Usuario@LAPTOP-M2PQ13GV>
2021-06-17 20:24:50 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T03_04
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = 8;
+            Console.WriteLine("N = " + N);
+            N += 77;
+            Console.WriteLine("N + 77 = " + N);
+            N -= 3;
+            Console.WriteLine("N - 3 = " + N);
+            N *= 2;
+            Console.WriteLine("N * 2 = " + N);
+        }
+    }
+}
